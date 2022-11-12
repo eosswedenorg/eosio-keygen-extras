d792c19d032b3344339ca372730a09db1d31eaa9
Henrik Hautakoski <henrik@eossweden.org>
2022-11-12 16:59:29 +0100

    update-dict.sh: Fix paths.

--- update-dict.sh
+++ update-dict.sh
@@ -3,8 +3,7 @@
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-EXTRAS_DIR=${BASE_DIR}/../extras
-CONFIG=${EXTRAS_DIR}/dict.json
+CONFIG=${BASE_DIR}/config.json
 
 for lang in $(jq -r 'keys[]' $CONFIG); do
 
@@ -14,12 +13,12 @@ for lang in $(jq -r 'keys[]' $CONFIG); do
 	for f in $(jq -r ".$lang | if type==\"array\" then .[] else . end" $CONFIG); do
 
 		if [ "${f:0:1}" != "/" ]; then
-			f="$EXTRAS_DIR/$f"
+			f="${BASE_DIR}/custom/$f"
 		fi
 		files+=( "$f" )
 	done
 
-	grep -hs ^ ${files[@]} | $BASE_DIR/generate-dict.sh > $EXTRAS_DIR/dict/$lang
+	grep -hs ^ ${files[@]} | $BASE_DIR/generate-dict.sh > $BASE_DIR/dict/$lang
 done
 
 echo "Done"
